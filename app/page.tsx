b8ff2f16daa204fd2320ab0c4a2a28f2490cca64
Steve Villardi <42367049+stevevillardi@users.noreply.github.com>
2024-11-24 04:39:22 -0500

    add speed insight

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import CollectorCalculator from './components/CollectorCalculator/index';
 import { Footer } from './components/CollectorCalculator/components/Footer';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#040F4B] flex flex-col">
       <main className="flex-grow p-8">
+        <SpeedInsights />
         <CollectorCalculator />
       </main>
       <Footer />
