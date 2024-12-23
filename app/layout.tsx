04f282f6b8e622dc1365158a1161993c8259d421
Steve Villardi <42367049+stevevillardi@users.noreply.github.com>
2024-12-23 12:08:58 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
       >
         {/* <ThemeProvider> */}
           {children}
-        </ThemeProvider>
+        {/* </ThemeProvider> */}
         <PWAInstallPrompt />
         <Analytics/>
       </body>
