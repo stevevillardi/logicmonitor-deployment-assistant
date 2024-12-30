eeb5776038ad9b9c4e8af387b61a904950493ba8
Steve Villardi <42367049+stevevillardi@users.noreply.github.com>
2024-12-30 17:40:39 -0500

    Update route.ts

--- app/api/dashboards/sync/route.ts
+++ app/api/dashboards/sync/route.ts
@@ -79,6 +79,9 @@ export async function GET(request: Request) {
   const authHeader = request.headers.get('authorization');
   const cronSecret = process.env.CRON_SECRET;
 
+console.log("auth:",authHeader)
+console.log("cron:",cronSecret)
+
   if (authHeader !== `Bearer ${cronSecret}`) {
     return NextResponse.json({ error: 'Unauthorized Sync' }, { status: 401 });
   }
